5557d0342c38d13db1d4b6756a541f6ea6487b7a
Muhammad Sheraz <mrsheraz325@gmail.com>
2026-08-31 23:57:24 +0500

    update files

--- client/src/pages/AIComposer.tsx
+++ client/src/pages/AIComposer.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from "react"
+import { dummyGenerationData } from "../assets/assets"
 
 
 function AIComposer() {
+  const [prompt, setPrompt] = useState("")
+  const [tone, setTone] = useState("Professional")
+  const [generateImage, setGenerateImage] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [generations, setGenerations] = useState<any[]>([])
+  
+  const [activeScheduler, setActiveScheduler] = useState<any>(null)
+  const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>([])
+  const [scheduledDate, setScheduledDate] = useState("")
+  const [scheduledTime, setScheduledTime] = useState("")
+
+  const [scheduling, setScheduling] = useState(false)
+
+  const fetchGenerations = async ()=>{
+    setGenerations(dummyGenerationData)
+  }
+  
+
+  useEffect(()=>{
+   fetchGenerations()
+  },[])
+
+const tones = [
+  "Professional",
+  "Casual",
+  "Funny",
+  "Creative",
+  "Excited",
+];
+
   return (
-    <div>AIComposer</div>
+    <div>
+
+    </div>
   )
 }
 
